d49fc3404f933c53893b6a4261015bede8b55cc8
Sejk_h2ym@student.itstep.org <Sejk_h2ym@student.itstep.org>
2020-11-04 23:13:56 +0200

    Add project files.

---
+++ homework_2105_guessnum/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace homework_2105_guessnum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(26, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Легко (3)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(26, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(98, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Нормально (5)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(26, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(79, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Тяжело (7)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(123, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Уровень";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "История";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Подтверждаю";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(152, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(139, 100);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Введите Число";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 152);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(276, 212);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Число";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(311, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Совпало цифр";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(311, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 31);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Совпало позиций";
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(411, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 17);
+            this.label6.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(411, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(411, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 17);
+            this.label7.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(297, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(253, 97);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "00:00:00";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(315, 260);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 33);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Количество попыток";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(412, 249);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 71);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "0";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 32);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "НОВАЯ ИГРА";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(544, 388);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess Number";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ homework_2105_guessnum/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework_2105_guessnum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Tick += (s, e) =>
+            {
+                label8.Text = $"{_starttime.Hour:00}:{_starttime.Minute:00}:{_starttime.Second:00}";
+                _starttime = _starttime.AddSeconds(1);
+            };
+            StartNewGame();
+            timer1.Start();
+        }
+
+        void GenerateNumber()
+        {
+            Random rand = new Random();
+            _cnumber.Clear();
+            int n = 0;
+            #region
+            //вариант1
+            while (n < q)
+            {
+                int num = rand.Next(1, 10);
+                if (!_cnumber.Contains(num))
+                {
+                    _cnumber.Add(num);
+                    n++;
+                }
+            }
+            #endregion
+
+            #region
+            //вариант 2
+            //List<int> tmprndlist=new List<int>();
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    tmprndlist.Add(i);
+            //}
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    int j = rand.Next(i + 1);
+            //    var temp = tmprndlist[j];
+            //    tmprndlist[j] = tmprndlist[i];
+            //    tmprndlist[i] = temp;
+            //}
+            //for (int i = 0; i < q; i++)
+            //{
+            //    _cnumber.Add(tmprndlist[i]);
+            //}
+            #endregion
+        }
+        void StringToList()
+        {
+            int unum = int.Parse(_usertext);
+            _unumber.Clear();
+            for (int i = q-1; i >=0; i--)
+            {
+                _unumber.Add((int)(unum / Math.Pow(10, i)));
+                unum = (int)(unum % Math.Pow(10, i));
+            }
+        }
+        bool CheckUserNumDup()
+        {
+            IEnumerable<int> unumbers = _unumber.Distinct();
+            if (unumbers.Count() == _unumber.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }    
+        }
+        void Contains()
+        {
+            _qcontainspos = _qcontainsnumber = 0;
+            List<int> containspos = new List<int>();
+            for (int i = 0; i < q; i++)
+            {
+                if (_cnumber.Contains(_unumber[i]))
+                { 
+                    _qcontainsnumber++;
+                    containspos.Add(i);
+                }
+            }
+            for (int i = 0; i < containspos.Count; i++)
+            {
+                if (_cnumber[containspos[i]] == _unumber[containspos[i]])
+                    _qcontainspos++;
+            }
+        }
+        void StartNewGame()
+        {
+            _cnumber.Clear();
+            _unumber.Clear();
+            _qcontainspos = 0;
+            _qcontainsnumber = 0;
+            _qtry = 0;
+            label5.Text = _qcontainspos.ToString();
+            label6.Text = "0";
+            label7.Text = _qcontainsnumber.ToString();
+            label10.Text = _qtry.ToString();
+            _starttime = new DateTime(1, 1, 1, 0, 0, 0, 0);
+            listBox1.Items.Clear();
+            listBox1.Items.Add($" Число \t    Совпало Цифр\t       Совпало позиций");
+            GenerateNumber();
+            textBox1.Text = "";
+        }
+
+
+        List<int> _cnumber = new List<int>();
+        List<int> _unumber = new List<int>();
+        DateTime _starttime = new DateTime(1, 1, 1, 0, 0, 0, 0);
+        int q = 3;
+        string _usertext = String.Empty;
+        int _qcontainspos;
+        int _qcontainsnumber;
+        int _qtry;
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _usertext = textBox1.Text;
+            StringToList();
+            if (!CheckUserNumDup())
+            {
+                MessageBox.Show("Число должно состоять только из уникальных цифр", "Ошибка");
+                textBox1.Text = "";
+            }
+            else if (_usertext.Length < q)
+            {
+                MessageBox.Show($"Количество введенных чисел меньше {q}", "Ошибка");
+                listBox1.Items.Add($" Число \t    Совпало Цифр\t       Совпало позиций");
+            }
+            else
+            {
+                label6.Text = textBox1.Text;
+                _qtry++;
+                Contains();
+                listBox1.Items.Add($"{_usertext}\t\t{_qcontainsnumber}\t\t {_qcontainspos}");
+                label5.Text = _qcontainspos.ToString();
+                label7.Text = _qcontainsnumber.ToString();
+                label10.Text = _qtry.ToString();
+                textBox1.Text = "";
+                if (q == _qcontainspos && q == _qcontainsnumber)
+                {
+                    MessageBox.Show($" Вы победили! Компьютер загадал число {_usertext}\n Попыток: {_qtry}\n Затрачено Времени: {label8.Text}\n", "Поздравляем!");
+                    textBox1.Text = "";
+                    StartNewGame();
+                }
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || e.KeyChar == '\b') || textBox1.TextLength >= q)
+                e.Handled = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            q = 3;
+            GenerateNumber();
+            StartNewGame();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            q = 5;
+            GenerateNumber();
+            StartNewGame();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            q = 7;
+            GenerateNumber();
+            StartNewGame();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
